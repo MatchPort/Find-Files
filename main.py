c7b624d1ca0df5ae73abfae92a6261d2f07faf68
MatchPort <131888743+MatchPort@users.noreply.github.com>
2023-06-03 23:59:04 -0400

    Update main.py with config support

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 import subprocess
+import configparser
 
 def open_file_explorer(path):
     path = os.path.realpath(path)
     subprocess.run(f'explorer /select,"{path}"')
 
-def find_env_files(path):
+def find_env_files(path, extension):
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith('.env'):
+            if file.endswith(extension):
                 full_path = os.path.join(root, file)
-                print(f'.env file found: {full_path}')
+                print(f'{extension} file found: {full_path}')
                 open_file_explorer(full_path)
 
-find_env_files('D:\\')
+def read_config():
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    return config['DEFAULT']['Path'], config['DEFAULT']['FileExtension']
+
+path, extension = read_config()
+find_env_files(path, extension)
